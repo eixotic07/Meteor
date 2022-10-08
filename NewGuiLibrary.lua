c5f109d16202b1edad53d8980d2ae2dda5e81b83
Superscripted#7327 <80987982+supercellgamer@users.noreply.github.com>
2022-10-07 21:20:14 -0600

    Update NewGuiLibrary.lua

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -19,7 +19,7 @@ if (not isfile("Meteor/scripts")) then
 end
 local MeteorNotifications = loadstring(game:HttpGet("https://raw.githubusercontent.com/eixotic07/Meteor/main/MeteorNotification.lua"))()
 -- { Version } --
-local METEORVER = '2.62'
+local METEORVER = '#intentratted'
 local IndentLevel1 = 8
 local IndentLevel2 = 14
 local IndentLevel3 = 22
