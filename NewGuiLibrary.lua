43595f866a19b8708928d611917c081133b65087
Superscripted#7327 <80987982+supercellgamer@users.noreply.github.com>
2022-10-12 22:56:55 -0600

    Update NewGuiLibrary.lua

--- NewGuiLibrary.lua
+++ NewGuiLibrary.lua
@@ -18,7 +18,7 @@ if (not isfile("Meteor/scripts")) then
     makefolder("Meteor/scripts")
 end
 -- { Version } --
-local METEORVER = '2.83'
+local METEORVER = '2.93'
 local IndentLevel1 = 8
 local IndentLevel2 = 14
 local IndentLevel3 = 22
